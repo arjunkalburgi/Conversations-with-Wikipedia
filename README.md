cc7ebf8eaff9ba3ac53815ab7cb05e5077ef13c6
Arjun Kalburgi <askalburgi@gmail.com>
2018-03-07 11:04:31 -0700

    implemented hidden keys, and topic sanitization

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # uncover_discovery
 The final project in the discovery line. 
 
+### Build instructions 
+ - `pip install wikipedia`
+ - `pip install watson_developer_cloud`
 
+### Run instructions 
+ - `python learn.py`
--- dependencies/agent.py
+++ dependencies/agent.py
@@ -20,5 +20,24 @@ class conversational_agent():
         self.watsonobj = watson(username=username, password=password, version="2017-02-27")
 
     def prompt(self): 
-        self.raw_topic = raw_input("Hello, what would you like to learn about? \n>>> ")
+        raw_topic = raw_input("Hello, what would you like to learn about? \n>>> ")
+
+        # set proper topic 
+        self.__getTopic(raw_topic)
+
+    def __getTopic(self, topic):
+        # if match
+        if topic in wikipedia.search(topic):
+            print("Pulling from wiki page on " + topic + ".")
+            self._topic = topic
+        # if close match
+        elif wikipedia.suggest(topic) is not None:
+            print("Pulling from " + wikipedia.suggest(topic) +
+                  ", if this isn't correct please exit and specify a different query.")
+            self._topic = wikipedia.suggest(topic)
+        # no matches
+        else:
+            print("That topic didn't work, please try again")
+            exit()
+
 
