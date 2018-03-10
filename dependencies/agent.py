c88a875ba8512ae581b94668160fe5aef2c9967d
Arjun Kalburgi <askalburgi@gmail.com>
2018-03-10 15:56:11 -0700

    completed narrower stage 1

--- dependencies/agent.py
+++ dependencies/agent.py
@@ -21,6 +21,7 @@ class conversational_agent():
     def __init__(self): 
         self.watsonobj = watson(username=username, password=password, version="2017-02-27")
         self.wiki_wiki = wikipediaapi.Wikipedia('en')
+        self.interestList = []
 
     def prompt(self): 
         raw_topic = input("What would you like to learn about? \n>>> ")
@@ -35,7 +36,7 @@ class conversational_agent():
         # set proper topic 
         self.__getTopic(raw_topic)
 
-        self.present_options()
+        self.__present_options()
 
     def __getTopic(self, topic):
         # if match
@@ -55,8 +56,7 @@ class conversational_agent():
             print("That topic didn't work, please try again")
             exit()
 
-
-    def present_options(self): 
+    def __present_options(self): 
         choices = [section.title for section in self.wiki_wiki_page.sections]
         choices.remove("See also")
         choices.remove("References")
@@ -70,4 +70,31 @@ class conversational_agent():
         ]
 
         answers = inquirer.prompt(questions)
-        print(answers) 
+
+        self.interestList = self.interestList + [self._topic + "::" + interest for interest in answers[self._topic]]
+
+        if len(answers[self._topic]) > 1:
+            self.__askfororder(answers[self._topic])
+        elif len(self.interestList) > 1:
+            self.__askfororder(self.interestList)
+        else:
+            self.__startLearning(self.interestList[0])
+
+    def __askfororder(self, interests):
+        questions = [
+            inquirer.List("learnnow",
+                            message="Which would you like to learn about first?",
+                            choices=interests,
+                            ),
+        ]
+
+        answer = inquirer.prompt(questions)
+
+        self.__startLearning(answer)
+
+    def __startLearning(self, section):
+        # does have subsections? 
+            # present subsection options 
+            # answer depth vs bredth 
+
+        # start learning 
