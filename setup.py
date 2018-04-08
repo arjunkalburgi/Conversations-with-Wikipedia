ebe48eea5704912f0f0411ffd050491fb4a64159
Arjun Kalburgi <askalburgi@gmail.com>
2018-04-08 15:07:32 -0600

    setup

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from distutils.core import setup
 
 setup(
-    name='uncover_discover',
+    name='Conversations-with-Wikipedia',
     author='Arjun Kalburgi',
     author_email='askalburgi@gmail.com',
-    url='https://github.com/askalburgi/uncover_discovery',
+    url='https://github.com/askalburgi/Conversations-with-Wikipedia',
     description='CLI learning application',
     long_description=open('README.md').read(),
 
     version='0.1dev',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     
-    packages=['uncover_discover', ],
+    packages=['Conversations-with-Wikipedia', 'Conversations-with-Wikipedia.dependencies'],
     install_requires=[
         'wikipedia',
+        'wikipediaapi',
+        'inquirer', 
+        'random',
         'watson_developer_cloud'
     ],
 )
